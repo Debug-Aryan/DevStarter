9d7476cbebed54fe1b86e3a55dd24300d7d74130
Debug-Aryan <aryanclg33@gmail.com>
2026-02-14 18:00:48 +0530

    Update Footer.jsx

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -1,4 +1,4 @@
-import { Github, Twitter, Linkedin, Heart, ExternalLink } from 'lucide-react';
+import { Github, Twitter, Linkedin, Laptop, Heart, ExternalLink } from 'lucide-react';
 import devstarterLogo from '../../assets/devstarter.svg';
 
 export default function Footer() {
@@ -8,10 +8,10 @@ export default function Footer() {
                 <div className="max-w-7xl mx-auto">
 
                     {/* Top Section */}
-                    <div className="flex flex-col md:flex-row justify-between items-center space-y-6 md:space-y-0">
+                    <div className="flex flex-col border-b border-gray-800 mb-4 py-4 md:flex-row justify-between items-center space-y-6 md:space-y-0">
 
                         {/* Left: Logo */}
-                        <div className="flex items-center space-x-3 group">
+                        <div className="flex items-center  space-x-3 group">
                             <div className="bg-white/5 rounded-lg flex items-center justify-center transform transition-transform duration-700 ease-in-out group-hover:rotate-[720deg]">
                                 <img
                                     src={devstarterLogo}
@@ -23,33 +23,37 @@ export default function Footer() {
                             <span className="text-xl font-bold font-mono text-white">DevStarter</span>
                         </div>
 
-                        {/* Center: Links */}
-                        <div className="flex flex-wrap justify-center md:justify-center space-x-6">
-                            <a href="#" className="text-gray-400 hover:text-white transition-colors text-sm md:text-base">About</a>
-                            <a href="#" className="text-gray-400 hover:text-white transition-colors text-sm md:text-base">GitHub</a>
-                            <a href="#" className="text-gray-400 hover:text-white transition-colors text-sm md:text-base">Contact</a>
+                        {/* Bottom */}
+                        <div className=" text-sm text-gray-500 text-center">
+                            {/* <p className="text-m text-gray-500">
+                                Empowering developers worldwide with modern tools • © {new Date().getFullYear()} DevStarter. All rights reserved.
+                            </p> */}
+                            <p className="hidden md:block text-m text-gray-500">
+                                Empowering developers worldwide with modern tools •
+                                © {new Date().getFullYear()} DevStarter •
+                                Built by <span className="font-semibold text-gray-300 hover:text-white transition-colors">Aryan Prajapati</span>
+                            </p>
+
+                            <p className="block md:hidden text-m text-gray-500">
+                                © {new Date().getFullYear()} DevStarter • <span className="font-semibold text-gray-300 hover:text-white transition-colors">Aryan Prajapati</span>
+                            </p>
                         </div>
 
                         {/* Right: Social Icons */}
                         <div className="flex space-x-5">
-                            <a href="#" className="text-gray-400 hover:text-white transition-colors">
+                            <a href="https://github.com/Debug-Aryan" className="text-gray-400 hover:text-white transition-colors">
                                 <Github className="w-6 h-6" />
                             </a>
-                            <a href="#" className="text-gray-400 hover:text-white transition-colors">
-                                <Twitter className="w-6 h-6" />
+                            <a href="https://www.linkedin.com/in/aryan-prajapati-b9b9a5284/" className="text-gray-400 hover:text-white transition-colors">
+                                <Linkedin className="w-6 h-6" />
                             </a>
                             <a href="#" className="text-gray-400 hover:text-white transition-colors">
-                                <Linkedin className="w-6 h-6" />
+                                <Laptop className="w-6 h-6" />
                             </a>
                         </div>
                     </div>
 
-                    {/* Bottom */}
-                    <div className="border-t border-gray-800 mt-8 pt-6 text-sm text-gray-500 text-center">
-                        <p className="text-m text-gray-500">
-                            Empowering developers worldwide with modern tools • © {new Date().getFullYear()} DevStarter. All rights reserved.
-                        </p>
-                    </div>
+
                 </div>
             </footer>
         </>
