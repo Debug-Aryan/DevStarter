63b95fb64c02112bbb9a88d756e982d307506c91
Debug-Aryan <aryanclg33@gmail.com>
2026-02-20 11:28:41 +0530

    Integrated Smooth Cursor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import LoadingOverlay from "./components/common/LoadingOverlay";
+import SmoothCursor from "./components/common/SmoothCursor";
 
 
 function App() {
   return (
     <BrowserRouter>
       <LoadingOverlay />
+      <SmoothCursor />
       <div className="min-h-screen flex flex-col">
         <main className="flex-grow">
           <AppRoutes />
